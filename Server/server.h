081f1c003ba11b16466d2d265a73d58f6b42e913
SinFox <avangard.jazz@gmail.com>
2019-07-04 23:00:39 +0300

    todo

--- Server/server.h
+++ Server/server.h
@@ -13,7 +13,7 @@ class Server
 
 private:
     int tcpSoc = -1;
-    int udpSoc = -1;
+    int udpSoc = -1; // do list of sockets for UDP
     struct sockaddr_in tcpSockAddr;
     struct sockaddr_in udpSockAddr;
     std::vector<int> connectedSockets;
