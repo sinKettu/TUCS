dfa7121c94f286a98fcea7c6a8e82e62a05ae5ed
SinFox <avangard.jazz@gmail.com>
2019-07-04 21:53:09 +0300

    Server: main() with serving loop

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,1 +1,21 @@
 #include "server.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello!\n");
+    printf("Test TCP & UDP server is starting...\n");
+    Server server;
+    
+    if (!server.Init())
+    {
+        printf("Bye!\n");
+        return 1;
+    }
+
+    printf("Success!\n");
+
+    while (server.Serve());
+
+    return 0;
+}
