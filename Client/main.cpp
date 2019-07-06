91b326a3cd7a358ed8de0c32bf843af3e9716f81
SinFox <avangard.jazz@gmail.com>
2019-07-06 09:24:50 +0300

    Main: parsing target address

--- Client/main.cpp
+++ Client/main.cpp
@@ -11,6 +11,29 @@ void Usage()
     std::cout << "'-a address:port' -- specifying target, optional flag\n";
 }
 
+bool GetTarget(std::string &address, unsigned long &port)
+{
+    std::cout << "Seems like you didn't specify target.\n";
+    std::cout << "Address: ";
+    std::getline(std::cin, address);
+    std::cout << "Port: ";
+    std::string tmp;
+    std::getline(std::cin, tmp);
+    size_t i = 0;
+    while (i < tmp.length() && tmp[i] >= '0' && tmp[i] <= '9') i++;
+    if (i < tmp.length())
+    {
+        return false;
+    }
+    port = strtoul(tmp.c_str(), NULL, 10);
+    if (port == 0 || port > UINT16_MAX)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc == 1)
@@ -76,19 +99,11 @@ int main(int argc, char *argv[])
         return 1;
     }
     std::cout << "Hello!\n";
-    std::cout << "Use CTRL+C to close program\n";
-    if (port == 0)
+    std::cout << "Use CTRL+C to close program.\n";
+    if (port == 0 && !GetTarget(address, port))
     {
-        std::cout << "Seems like you didn't specify target\n";
-        std::cout << "Address: ";
-        std::getline(std::cin, address);
-        std::cout << "Port: ";
-        std::cin >> port;
-        if (port == 0 || port > UINT16_MAX)
-        {
-            std::cout << "Wrong port\n";
-            return 1;
-        }
+        std::cout << "Wrong port.\n";
+        return 1;
     }
 
     Client client(type);
@@ -108,7 +123,7 @@ int main(int argc, char *argv[])
         std::string response = client.Receive();
         if (response.empty())
         {
-            std::cout << "Bad response\n";
+            std::cout << "Bad response.\n";
             continue;
         }
         std::cout << "Response:\n" << response << "\n";
